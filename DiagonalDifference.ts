5cfb9ccf12d3969544765dd8eef060b0f5ee103f
Mateo Sierra <m.sierra1037@hotmail.com>
2023-05-04 17:53:04 -0600

    Innitial Commit: Innitial configurations

---
+++ DiagonalDifference.ts
@@ -0,0 +1,13 @@
+function diagonalDifference(arr: number[][]): number {
+    let result: number = 0
+
+    for(let o = 0; o < arr.length; o++){
+        console.log(arr[o][0])
+    }
+
+
+    return result
+}
+
+
+console.log(diagonalDifference([[1,2,3],[4,5,6],[9,8,9]]))
